234d3300842211db98bbcb273bbccaa967fc2217
Daniela Kepper <dkepper@dkepper-macbookpro2.roam.corp.google.com>
2018-06-24 21:51:07 +0100

    first commit

---
+++ app/src/main/java/pawlliance/com/pawlliance/AppEntryPoint.java
@@ -0,0 +1,13 @@
+package pawlliance.com.pawlliance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppEntryPoint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_entry_point);
+    }
+}
