ff8cef93cd34cd639a899449b72bfc7f6389de78
Daniela Kepper <dkepper@dkepper-macbookpro2.roam.corp.google.com>
2018-07-08 17:41:18 +0100

    first step including validation to SignUpActivityPart1

--- app/src/main/java/pawlliance/com/pawlliance/activities/SignUpActivityPart1.java
+++ app/src/main/java/pawlliance/com/pawlliance/activities/SignUpActivityPart1.java
@@ -7,18 +7,41 @@
 package pawlliance.com.pawlliance.activities;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import org.w3c.dom.Text;
+
 import pawlliance.com.pawlliance.R;
+import pawlliance.com.pawlliance.helper.InputValidation;
+import pawlliance.com.pawlliance.sql.DatabaseHelper;
 
-public class SignUpActivityPart1 extends AppCompatActivity implements View.OnClickListener {
+public class SignUpActivityPart1 extends AppCompatActivity {
 
+    private final AppCompatActivity activity = SignUpActivityPart1.this;
 
+    // set all variables for input fields and layouts
+    private TextInputLayout signUpDogOwnersFullNameTextInputLayout;
+    private TextInputLayout signUpOwnersEmailTextInputLayout;
+    private TextInputLayout signUpOwnersPasswordTextInputLayout;
+    private TextInputLayout signUpOwnersConfirmPasswordTextInputLayout;
+
+    private Spinner signUpCitySpinner;
+
+    private TextInputEditText signUpOwnersFullNameTextInputEditText;
+    private TextInputEditText signUpOwnersEmailTextInputEditText;
+    private TextInputEditText signUpOwnersPasswordTextInputEditText;
+    private TextInputEditText signUpOwnersConfirmPasswordTextInputEditText;
+
+    private InputValidation inputValidation;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,28 +49,53 @@ public class SignUpActivityPart1 extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_signup_part1);
         getSupportActionBar().hide();
 
-
-        // SPINNER ACTION FOR CITY SELECTOR
-        Spinner staticSpinner = (Spinner) findViewById(R.id.SignUpCitySpinner);
-
-        // Create an ArrayAdapter using the string array and a default spinner
-        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(this, R.array.city_array, android.R.layout.simple_spinner_item);
-
-        // Specify the layout to use when the list of choices appears
-        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
-        staticSpinner.setAdapter(staticAdapter);
-
+        initViews();
+        initObjects();
 
         // setting up login button redirect to LoginActivity.class
         Button signUpNextPageButton = (Button) findViewById(R.id.SignUpNextPageButton);
         signUpNextPageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                checkInputValidation();
                 Intent signUpPage2Intent = new Intent(getApplicationContext(), SignUpActivityPart2.class);
                 startActivity(signUpPage2Intent);
             }
         });
     }
+
+    private void initViews(){
+        // TextInputLayouts
+        signUpDogOwnersFullNameTextInputLayout = (TextInputLayout) findViewById(R.id.SignUpDogOwnersFullNameTextInputLayout);
+        signUpOwnersEmailTextInputLayout = (TextInputLayout) findViewById(R.id.SignUpOwnersEmailTextInputLayout);
+        signUpOwnersPasswordTextInputLayout = (TextInputLayout) findViewById(R.id.SignUpOwnersPasswordTextInputLayout);
+        signUpOwnersConfirmPasswordTextInputLayout = (TextInputLayout) findViewById(R.id.SignUpOwnersConfirmPasswordTextInputLayout);
+
+        // TextInputEditTexts
+        signUpOwnersFullNameTextInputEditText = (TextInputEditText) findViewById(R.id.SignUpOwnersFullNameTextInputEditText);
+        signUpOwnersEmailTextInputEditText = (TextInputEditText) findViewById(R.id.SignUpOwnersEmailTextInputEditText);
+        signUpOwnersPasswordTextInputEditText = (TextInputEditText) findViewById(R.id.SignUpOwnersPasswordTextInputEditText);
+        signUpOwnersConfirmPasswordTextInputEditText = (TextInputEditText) findViewById(R.id.SignUpOwnersConfirmPasswordTextInputEditText);
+
+
+        // SPINNER ACTION FOR CITY SELECTOR
+        signUpCitySpinner = (Spinner) findViewById(R.id.SignUpCitySpinner);
+        // Create an ArrayAdapter using the string array and a default spinner
+        ArrayAdapter<CharSequence> staticAdapter = ArrayAdapter.createFromResource(this, R.array.city_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        staticAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        signUpCitySpinner.setAdapter(staticAdapter);
+
+    }
+
+    private void initObjects(){
+        inputValidation = new InputValidation(activity);
+        databaseHelper = new DatabaseHelper(activity);
+    }
+
+    private void checkInputValidation(){
+        
+    }
+
 }
