aa7222f990f1f40e7af1e05ac11c52af14e8fa53
Daniela Kepper <dkepper@dkepper-macbookpro2.roam.corp.google.com>
2018-07-08 18:55:03 +0100

    finished input validation for signup page 1

--- app/src/main/java/pawlliance/com/pawlliance/activities/SignUpActivityPart1.java
+++ app/src/main/java/pawlliance/com/pawlliance/activities/SignUpActivityPart1.java
@@ -7,6 +7,7 @@
 package pawlliance.com.pawlliance.activities;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -23,7 +24,7 @@ import pawlliance.com.pawlliance.R;
 import pawlliance.com.pawlliance.helper.InputValidation;
 import pawlliance.com.pawlliance.sql.DatabaseHelper;
 
-public class SignUpActivityPart1 extends AppCompatActivity {
+public class SignUpActivityPart1 extends AppCompatActivity implements View.OnClickListener {
 
     private final AppCompatActivity activity = SignUpActivityPart1.this;
 
@@ -40,6 +41,8 @@ public class SignUpActivityPart1 extends AppCompatActivity {
     private TextInputEditText signUpOwnersPasswordTextInputEditText;
     private TextInputEditText signUpOwnersConfirmPasswordTextInputEditText;
 
+    Button signUpNextPageButton;
+
     private InputValidation inputValidation;
     private DatabaseHelper databaseHelper;
 
@@ -50,18 +53,8 @@ public class SignUpActivityPart1 extends AppCompatActivity {
         getSupportActionBar().hide();
 
         initViews();
+        initListener();
         initObjects();
-
-        // setting up login button redirect to LoginActivity.class
-        Button signUpNextPageButton = (Button) findViewById(R.id.SignUpNextPageButton);
-        signUpNextPageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkInputValidation();
-                Intent signUpPage2Intent = new Intent(getApplicationContext(), SignUpActivityPart2.class);
-                startActivity(signUpPage2Intent);
-            }
-        });
     }
 
     private void initViews(){
@@ -77,6 +70,8 @@ public class SignUpActivityPart1 extends AppCompatActivity {
         signUpOwnersPasswordTextInputEditText = (TextInputEditText) findViewById(R.id.SignUpOwnersPasswordTextInputEditText);
         signUpOwnersConfirmPasswordTextInputEditText = (TextInputEditText) findViewById(R.id.SignUpOwnersConfirmPasswordTextInputEditText);
 
+        // Button
+        signUpNextPageButton = (Button) findViewById(R.id.SignUpNextPageButton);
 
         // SPINNER ACTION FOR CITY SELECTOR
         signUpCitySpinner = (Spinner) findViewById(R.id.SignUpCitySpinner);
@@ -89,13 +84,78 @@ public class SignUpActivityPart1 extends AppCompatActivity {
 
     }
 
+    /**
+     * This method is to initialize objects to be used
+     */
     private void initObjects(){
         inputValidation = new InputValidation(activity);
         databaseHelper = new DatabaseHelper(activity);
     }
 
-    private void checkInputValidation(){
-        
+    /**
+     * This method is to initialize listeners
+     */
+    public void initListener(){
+        signUpNextPageButton.setOnClickListener(this);
     }
 
+    /**
+     * This implemented method is to listen the click on view
+     *
+     * @param v
+     */
+     @Override
+     public void onClick(View v){
+         switch(v.getId()){
+             case R.id.SignUpNextPageButton:
+                 checkInputValidation();
+                 break;
+         }
+     }
+
+    private void checkInputValidation(){
+        if (!inputValidation.isInputEditTextFilled(signUpOwnersFullNameTextInputEditText, signUpDogOwnersFullNameTextInputLayout, getString(R.string.SignUpErrorMessageOwnersName))) {
+            return;
+        }
+        if(!inputValidation.isInputEditTextFilled(signUpOwnersEmailTextInputEditText, signUpOwnersEmailTextInputLayout, getString(R.string.SignUpErrorMessageEmail))) {
+            return;
+        }
+        if(!inputValidation.isInputEditTextEmail(signUpOwnersEmailTextInputEditText, signUpOwnersEmailTextInputLayout, getString(R.string.SignUpErrorMessageEmail))) {
+            return;
+        }
+        if(!inputValidation.isInputEditTextFilled(signUpOwnersPasswordTextInputEditText, signUpOwnersPasswordTextInputLayout, getString(R.string.SignUpErrorMessagePassword))) {
+            return;
+        }
+        if(!inputValidation.isInputEditTextMatches(signUpOwnersPasswordTextInputEditText, signUpOwnersConfirmPasswordTextInputEditText,
+                signUpOwnersConfirmPasswordTextInputLayout, getString(R.string.SignUpErrorMessagePasswordMatch))) {
+            return;
+        }
+        if(!databaseHelper.checkUser(signUpOwnersEmailTextInputEditText.getText().toString().trim())) {
+            // setting up login button redirect to LoginActivity.class
+            signUpNextPageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    emptyInputEditText();
+                    Intent signUpPage2Intent = new Intent(getApplicationContext(), SignUpActivityPart2.class);
+                    startActivity(signUpPage2Intent);
+                }
+            });
+        }
+        else {
+            // Snack Bar to show error message that record already exists
+            Snackbar.make(signUpOwnersEmailTextInputLayout, getString(R.string.SignUpErrorMessageEmailExists), Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * This method is to empty all input edit text
+     */
+    private void emptyInputEditText(){
+        signUpOwnersFullNameTextInputEditText.setText(null);
+        signUpOwnersEmailTextInputEditText.setText(null);
+        signUpOwnersPasswordTextInputEditText.setText(null);
+        signUpOwnersConfirmPasswordTextInputEditText.setText(null);
+    }
+
+    // DNC - class closing
 }
